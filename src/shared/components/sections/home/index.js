0d171dd2a3a57351022ed7e69eff3d1366781318
ari <garciadiazivanari@gmail.com>
2016-02-18 13:55:14 -0800

    data en home

--- src/shared/components/sections/home/index.js
+++ src/shared/components/sections/home/index.js
@@ -3,11 +3,12 @@ import React from 'react';
 import Block1 from './block1';
 import Block2 from './block2';
 import Block3 from './block3';
+import data from './data';
 
 export default class HomeSection extends React.Component {
   render() {
     return (<div>
-      <Block1 />
+      <Block1 data={data.block1} />
       <Block2 />
       <Block3 />
       </div>);
